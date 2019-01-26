d8a4d5568581ec014051efc6a86b7fe2f60c7584
Tom <tdostilio@gmail.com>
2019-01-26 16:51:08 -0500

    adds CRUD operations

--- backend/lib/routes/contact.js
+++ backend/lib/routes/contact.js
@@ -13,16 +13,10 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/contacts", (req, res) => {
-  res.status(200).send({
-    success: "true",
-    message: "hello world"
-  });
-});
-
+// Create new contact
 app.post("/contact", async (req, res) => {
-  if (!req.body.name) {
-    throw new Error("No name provided");
+  if (!req.body.name || !req.body.relationship) {
+    res.status(404).send("Missing parameters");
   }
   let newContact = new Contact({
     name: req.body.name,
@@ -38,6 +32,51 @@ app.post("/contact", async (req, res) => {
   res.status(200).send({
     success: "true",
     message: newContact.name
+  });
+});
+
+// Read an existing contact
+app.get("/contact/:id", async (req, res) => {
+  let contact;
+  try {
+    contact = await Contact.find({ _id: req.params.id }, err => {
+      if (err) {
+        res.status(500).send("Server error");
+      }
+    });
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
+
+  res.status(200).send({
+    body: contact
+  });
+});
+
+// Update an existing contact
+app.put("/contact/:id", async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+    contact.name = req.body.name;
+    contact.save();
+  } catch (err) {
+    res.status(500).send(`Error updating contact: ${err}`);
+  }
+  res.status(200).send({
+    body: "completed"
+  });
+});
+
+app.delete("/contact/:id", async (req, res) => {
+  try {
+    let contact = await Contact.findByIdAndDelete(req.params.id);
+    contact.name = req.body.name;
+    contact.save();
+  } catch (err) {
+    res.status(500).send(`Error updating contact: ${err}`);
+  }
+  res.status(200).send({
+    body: "completed"
   });
 });
 
